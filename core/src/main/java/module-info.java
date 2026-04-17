6efd24defc1bb941ae68d03427084b72b52a83da
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 16:25:59 +0300

    reduce ide.api deps

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,3 +1,21 @@
 module org.toml.lang {
-    requires consulo.ide.api;
+    requires consulo.language.api;
+    requires consulo.language.impl;
+    requires consulo.language.editor.api;
+    requires consulo.language.code.style.api;
+    requires consulo.language.code.style.ui.api;
+
+    requires consulo.application.api;
+    requires consulo.code.editor.api;
+    requires consulo.color.scheme.api;
+    requires consulo.configurable.api;
+    requires consulo.disposer.api;
+    requires consulo.document.api;
+    requires consulo.navigation.api;
+    requires consulo.project.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
+    requires consulo.virtual.file.system.api;
+    requires consulo.util.io;
+    requires consulo.util.lang;
 }
