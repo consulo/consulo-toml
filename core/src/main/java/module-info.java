a4788fb0f61d22484642001ffce5496fd84e6cdf
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-05-16 18:51:33 +0300

    add open

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -18,4 +18,28 @@ module org.toml.lang {
     requires consulo.virtual.file.system.api;
     requires consulo.util.io;
     requires consulo.util.lang;
+
+    exports consulo.toml.icon;
+    exports consulo.toml.ide;
+    exports consulo.toml.ide.annotator;
+    exports org.toml.ide;
+    exports org.toml.ide.annotator;
+    exports org.toml.ide.colors;
+    exports org.toml.ide.experiments;
+    exports org.toml.ide.folding;
+    exports org.toml.ide.formatter;
+    exports org.toml.ide.formatter.impl;
+    exports org.toml.ide.formatter.settings;
+    exports org.toml.ide.inspections;
+    exports org.toml.ide.intentions;
+    exports org.toml.ide.resolve;
+    exports org.toml.ide.search;
+    exports org.toml.ide.todo;
+    exports org.toml.ide.wordSelection;
+    exports org.toml.lang;
+    exports org.toml.lang.lexer;
+    exports org.toml.lang.parse;
+    exports org.toml.lang.psi;
+    exports org.toml.lang.psi.ext;
+    exports org.toml.lang.psi.impl;
 }
